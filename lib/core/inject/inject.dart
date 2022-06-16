39ea92d12d585042b2289660f419bf917f0e4f68
reinans <reinandesouza01@gmail.com>
2022-06-15 22:37:37 -0300

    fix: update inject with apod list

--- lib/core/inject/inject.dart
+++ lib/core/inject/inject.dart
@@ -1,9 +1,9 @@
-import 'package:apod_app/data/datasources/get_apod_datasource.dart';
-import 'package:apod_app/data/datasources/local/get_apod_datasource_decorator_impl.dart';
-import 'package:apod_app/data/datasources/remote/get_apod_remote_datasource_imp.dart';
-import 'package:apod_app/data/repositories/get_apod_repository_imp.dart';
-import 'package:apod_app/domain/repositories/get_apod_repository.dart';
-import 'package:apod_app/domain/use_cases/get_apod_usecase.dart';
+import 'package:apod_app/data/datasources/get_list_apod_datasource.dart';
+import 'package:apod_app/data/datasources/local/get_list_apod_datasource_decorator_impl.dart';
+import 'package:apod_app/data/datasources/remote/get_list_apod_remote_datasource_imp.dart';
+import 'package:apod_app/data/repositories/get_list_apod_repository_imp.dart';
+import 'package:apod_app/domain/repositories/get_list_apod_repository.dart';
+import 'package:apod_app/domain/use_cases/get_list_apod_usecase.dart';
 import 'package:apod_app/presentation/controllers/apod_controller.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
@@ -16,20 +16,20 @@ class Inject {
     getIt.registerLazySingleton(() => http.Client());
 
     // datasources
-    getIt.registerLazySingleton<GetApodDatasource>(
-      () => GetApodDataSourceDecoratorImpl(
-        GetApodRemoteDataSourceImp(client: getIt()),
+    getIt.registerLazySingleton<GetListApodDatasource>(
+      () => GetListApodDataSourceDecoratorImpl(
+        GetListApodRemoteDataSourceImp(client: getIt()),
       ),
     );
 
     // repositories
-    getIt.registerLazySingleton<GetApodRepository>(
-      () => GetApodRepositoryImpl(getIt()),
+    getIt.registerLazySingleton<GetListApodRepository>(
+      () => GetListApodRepositoryImpl(getIt()),
     );
 
     // usecases
-    getIt.registerLazySingleton<GetApodUseCase>(
-      () => GetApodUseCase(getIt()),
+    getIt.registerLazySingleton<GetListApodUseCase>(
+      () => GetListApodUseCase(getIt()),
     );
 
     // controllers
