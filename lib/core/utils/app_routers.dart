17453115fbf5ff36d416ae407e45f15ba10c5091
reinans <reinandesouza01@gmail.com>
2022-06-16 22:21:09 -0300

    feat: add api constants

--- lib/core/utils/app_routers.dart
+++ lib/core/utils/app_routers.dart
@@ -1,7 +1,10 @@
 import 'package:apod_app/domain/entities/apod_entity.dart';
+import 'package:apod_app/presentation/controllers/apod_controller.dart';
+import 'package:apod_app/presentation/controllers/apod_details_controller.dart';
 import 'package:apod_app/presentation/ui/pages/apod_details_page.dart';
 import 'package:apod_app/presentation/ui/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 const String homeRoute = '/home';
 const String apodDetailsRoute = '/apod/details';
@@ -12,17 +15,29 @@ class AppRouters {
 
     switch (settings.name) {
       case homeRoute:
-        return MaterialPageRoute(builder: (_) => const HomePage());
+        return MaterialPageRoute(
+          builder: (_) => HomePage(
+            apodController: GetIt.I.get<ApodController>(),
+          ),
+        );
 
       case apodDetailsRoute:
         if (args is ApodDetailsArgs) {
           return MaterialPageRoute(
-              builder: (_) => ApodDetailsPage(apodEntity: args.apodEntity));
+            builder: (_) => ApodDetailsPage(
+              apodEntity: args.apodEntity,
+              apodDetailsController: GetIt.I.get<ApodDetailsController>(),
+            ),
+          );
         }
         break;
 
       default:
-        return MaterialPageRoute(builder: (_) => const HomePage());
+        return MaterialPageRoute(
+          builder: (_) => HomePage(
+            apodController: GetIt.I.get<ApodController>(),
+          ),
+        );
     }
   }
 }
